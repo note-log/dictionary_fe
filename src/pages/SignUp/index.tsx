e87a5a75d5c9500f217404f6bb782f94241033b9
SnowWarri0r <gerrytranCHINA@gmail.com>
2023-06-18 17:35:48 +0800

    fix typo

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -58,7 +58,7 @@ export default function SignUp() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign up
+            注册
           </Typography>
           <Box
             component="form"
